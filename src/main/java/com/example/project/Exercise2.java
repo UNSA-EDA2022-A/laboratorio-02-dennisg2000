21bcda87e26eccefb501b3d399b28341e37dd60d
dennisg2000 <84654444+dennisg2000@users.noreply.github.com>
2022-05-15 15:14:36 -0500

    Update Exercise2.java

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -9,23 +9,42 @@ public class Exercise2 {
 		Exercise2 obj = new Exercise2();
 		Scanner sc = new Scanner(System.in);
 		while (true) {
+			int n;
+			int p = sc.nextInt(), suma;
+			int a[] = new int[p];
 
-			int n = sc.nextInt(), suma;
-			int a[] = new int[n];
-
-			for (int i = 0; i < n; i++) {
+			for (int i = 0; i < p; i++) {
 				n = sc.nextInt();
 				a[i] = n;
 			}
 
 			suma = sc.nextInt();
-
-			System.out.println(obj.esSubconjuntoSumaExt(a, suma));
+			System.out.println(obj.esSubconjuntoSumaExt(a,suma));
 		}
 	}
 
-	public boolean esSubconjuntoSumaExt(int a[], int suma) {
-
-		return false;
+	public boolean esSubconjuntoSumaExt(int a[],int suma) {
+		
+		return esSubconjuntoSumaExt(a, 0, suma ); 
+	}
+	
+	public static boolean esSubconjuntoSumaExt(int a[], int n,int suma) {
+		if (suma == 0 )
+			return true;
+		if (n == a.length)
+			return false;
+		if(a[n]%7!=0) {
+			
+				
+			 return esSubconjuntoSumaExt(a, n + 1, suma - a[n])
+				|| esSubconjuntoSumaExt(a, n+ 1, suma );
+		}
+		else {
+			if(a[n+1]==1) {
+				
+				return esSubconjuntoSumaExt(a, n + 1, suma);
+			}else return esSubconjuntoSumaExt(a, n + 1, suma - a[n]);
+			
+		}
 	}
 }
